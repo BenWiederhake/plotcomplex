9e299386416db2e2408ad649cadaee29777fcd71
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2017-04-29 09:24:56 +0200

    Use complex library

--- plotcomplex.py
+++ plotcomplex.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python3
 
 from PIL import Image
-from math import atan2, floor, pi
+from cmath import polar, pi
 from colorsys import hsv_to_rgb
 
 
-def as_col(r, i):
-    val = r * r + i * i
-    phi = atan2(i, r) / (2 * pi) % 1
-    assert phi >= -0.0001, phi
-    assert phi <= 1.0001, phi
+def as_col(c):
+    (val, phi) = polar(c)
+    phi = phi / (2 * pi) % 1
     if val <= 1.0:
-        rgb = hsv_to_rgb(phi, 1, (2 - val) * val)
+        rgb = hsv_to_rgb(phi, 1, val)
     else:
         rgb = hsv_to_rgb(phi, 1 / val, 1)
     return tuple([round(c * 255) for c in rgb])
 
 
-def f(r, i):
+
+def f(c):
     # Identity
-    return (r, i)
+    return c
 
 
 W = 640
@@ -34,7 +33,7 @@ if __name__ == '__main__':
         r = (x + OFFSET_X) * SCALE
         for y in range(H):
             i = (y + OFFSET_Y) * SCALE
-            (r, i) = f(r, i)
-            img.putpixel((x, y), as_col(r, i))
+            c = f(r + i * 1j)
+            img.putpixel((x, y), as_col(c))
     img.show()
     img.save("myimg.png")
